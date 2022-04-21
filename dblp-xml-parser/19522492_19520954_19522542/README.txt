2b29bd29d6b262bf86887bc28b629ad1706b7a43
Tung Huynh Thien <19522492@gm.uit.edu.vn>
2022-04-21 21:54:36 +0700

    aaaa

---
+++ dblp-xml-parser/19522492_19520954_19522542/README.txt
@@ -0,0 +1,31 @@
+BÀI TẬP LAB 2: Viết Chương trình đọc thông tin từ file xml của trang dblp sau đó lưu xuống một CSDL quan hệ về các bài báo khoa học.
+
+Sinh viên thực hiện:
+	1. HUỲNH THIỆN TÙNG 	19522492
+	2. NGUYỄN THÀNH VƯƠNG 	19522542
+	3. LÊ THỊ THANH THANH	19520954
+
+Link repository trên github của nhóm: https://github.com/tuilatung/CS232/dblp-xml-parser
+
+
+
+1. Các thư viện sử dụng: 
+- Built-in: lxml.etree, pathlib.Path, sqlite3
+- Others: pandas, unidecode
+
+2. Nếu có lỗi thiếu thư viện:
+	+ pip install pandas
+	+ pip install Unidecode
+
+3. Chú ý change path của file dblp.xml và file parsed_data.csv cũng như file dblp.db
+   Thông thường khi giải nén chúng ta nên kiểm tra và sửa đường dẫn cho đúng
+
+4. Làm sao để chạy?
+	+ Chạy file main.py trước để có được file parsed_data.csv
+	+ Sau đó chạy file convert2sqlite.py để tạo ra file database có tên dblp.db
+
+
+5. Yêu cầu:
+	+ Python 3
+	+ Tải file dblp.xml từ trang: https://dblp.org/xml/
+	+ Cài đặt các thư viện đã liệt kê trên mục 1
--- python_image_crawler/google_image_crawler.py
+++ python_image_crawler/google_image_crawler.py
@@ -12,7 +12,9 @@ import json
 
 QUERY_STRING = 'ChiPu'
 NUMBER_OF_IMAGE_TO_SAVE = 100
-HIDE_DRIVER_WINDOWS = True
+HIDE_DRIVER_WINDOWS = False
+
+
 
 # Create browser object
 browser = webdriver.Chrome(service=Service('./chromedriver.exe'))
@@ -38,6 +40,8 @@ sleep(2)
 box.send_keys(Keys.ENTER)
 sleep(1)
 
+
+
 print('Log: Scrolling to the bottom of the page')
 # Auto scrolling to the bottom of the page
 scroll_to_bottom(browser)
@@ -46,14 +50,14 @@ scroll_to_bottom(browser)
     we need to loop through all of image and save them"""
 browser.find_element(by=By.TAG_NAME, value='body').send_keys(Keys.CONTROL + Keys.HOME)
 
+
 urls = get_image_urls(browser, number_of_image=NUMBER_OF_IMAGE_TO_SAVE)
 print(urls)
 
-dict_image_urls = {str(idx): url for idx, url in enumerate(urls)}
 
+dict_image_urls = {str(idx): url for idx, url in enumerate(urls)}
 with open('image_urls.json', 'w', encoding='utf-8') as f:
     json.dump(dict_image_urls, f, indent=4)
-
 os.makedirs(os.path.join(os.getcwd(), QUERY_STRING), exist_ok=True)
 for i, url in enumerate(urls):
     download_images('./' + QUERY_STRING + '/', url, QUERY_STRING + str(i + 1) + '.jpg')
